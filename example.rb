bb9122244b1244a5149d366761b3238928edb8fa
Julian Cheal <julian.cheal@gmail.com>
2014-07-31 21:05:13 +0300

    Made a couple of examples

--- example.rb
+++ example.rb
@@ -1,7 +1,33 @@
 require './lib/lpd8806'
 
-strip = Lpd8806.new("/dev/cu.usbserial-A600aeYe")
+@strip = Lpd8806.new("/dev/cu.usbserial-A600aeYe")
 
-strip.set_packed_pixel("a",0x222)
-sleep(5)
-strip.set_packed_pixel("a",0x00)
+# def stack(leds)
+#   @strip.set_packed_pixel(0,0x222)
+#   leds=leds-1
+#   leds.downto(0) do |l|
+#     # puts "l: #{l}"
+#     (1..l).each do |i|
+#       previous = i-1
+#       sleep(0.1)
+#       @strip.set_packed_pixel(previous,0x00)
+#       @strip.set_packed_pixel(i,0x222)
+#       # puts "previous: #{previous}"
+#       # puts "i: #{i}"
+#     end
+#   end
+# end
+# 
+# stack(32)
+
+# def random(leds)
+#   
+#   leds = (0...leds).to_a
+#   @strip.set_packed_pixel(leds.sample,0x222)
+#   sleep(0.3)
+#   @strip.set_packed_pixel("a",0x00)
+# end
+# 
+# (1..10).each do |i|
+#   random(32)
+# end
