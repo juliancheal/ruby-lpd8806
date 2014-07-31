d10e046db1730ca582ef6ab53898211007331bb4
Julian Cheal <julian.cheal@gmail.com>
2014-07-31 19:24:14 +0300

    Now using Hybridgroup-Firmarta.
    Using Hybridgroup-Firmarta although, I had to change their code to accept raw stream of bytes, will check with @zanick if I need to do anyting different.
    
    So far can send a hex rgb value and an LED position. It's basically at the same point as the node version I've written. Now I just need to make the firmware better.

--- lpd8806.rb
+++ lpd8806.rb
@@ -1,29 +1,25 @@
 require 'bundler/setup'
 require 'firmata'
 require 'socket'
+require 'stringio'
 
 #sp = 'COM3' # windows
 #sp = '/dev/ttyACM0' #linux
-sp = '/dev/tty.usbserial-A600aeYe' #mac
+sp = '/dev/cu.usbserial-A600aeYe' #mac
 
-board = Firmata::Board.new(sp)
+@board = ::Firmata::Board.new(sp)
 
-board.connect
+@board.connect
 
-puts "Firmware name #{board.firmware_name}"
-puts "Firmata version #{board.version}"
+puts "Firmware name #{@board.firmware_name}"
+puts "Firmata version #{@board.version}"
 
 data_pin = 2
 clock_pin = 3
 stip_lengh = 32
-
-board.digital_write(data_pin, Firmata::PinLevels::HIGH)
-board.digital_write(clock_pin, Firmata::PinLevels::HIGH)
-
-def set_pixel(position, colour)
-  message = "{p: #{position}, c: #{colour}}"
-  sys_message(message)
-end
+# board.set_pin_mode(2, 0x01)
+# board.set_pin_mode(data_pin, ::Firmata::PinModes::OUTPUT)
+# board.set_pin_mode(clock_pin, ::Firmata::PinModes::OUTPUT)
 
 def show
 end
@@ -33,12 +29,24 @@ def sys_message(message)
   string_data = 0x71
   end_sysex   = 0xF7
   data = []
-
+  
   data.push(start_sysex)
   data.push(string_data)
-  data.push(message + '\0')
+  bytes = StringIO.new(String(message+'\0'))
+
+  bytes.each_byte do |byte|
+    data.push(byte & 0x7F)
+    data.push((byte >> 7) & 0x7F)
+  end
   data.push(end_sysex)
   
-  board.read_and_process(data)
-  puts data
+  @board.write_and_process(data)
 end
+
+def set_packed_pixel(position, colour)
+  message = "{p:#{position},c:#{colour}}"
+  puts message
+  sys_message(message)
+end
+
+set_packed_pixel("a",0x222)
